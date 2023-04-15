39f29552c8eae0b803158c84d82ef35ee6549ee1
Daniel Vasti <danielvasticr@gmail.com>
2023-04-15 17:33:33 -0300

    CRUD FUNCIONANDO

---
+++ Academico/Controllers/DepartamentoController.cs
@@ -0,0 +1,143 @@
+﻿using Academico.Data;
+using Academico.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Academico.Controllers
+{
+
+    public class DepartamentoController : Controller
+    {
+
+        public bool DepartamentoExists(long? id)
+        {
+            return _context.Departamentos.Any(i => i.DepartamentoId == id);
+        }
+
+        private readonly AcademicoContext _context;
+
+        public DepartamentoController(AcademicoContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Departamentos.OrderBy(i => i.Nome).ToListAsync());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public async Task<ActionResult> Create([Bind("Nome")] Departamento departamento)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _context.Add(departamento);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (DbUpdateException)
+            {
+                ModelState.AddModelError("", "Não foi possível cadastrar o departamento");
+            }
+            return View(departamento);
+        }
+
+        public async Task<IActionResult> Edit(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var departamento = await _context.Departamentos.SingleOrDefaultAsync(i => i.DepartamentoId == id);
+            if (departamento == null)
+            {
+                return NotFound();
+            }
+            return View(departamento);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public async Task<IActionResult> Edit(long? id, [Bind("Nome")] Departamento departamento)
+        {
+            if (id != departamento.DepartamentoId)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(departamento);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DepartamentoExists(departamento.DepartamentoId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+            return View(departamento);
+        }
+
+        public async Task<IActionResult> Details(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var departamento = await _context.Instituicoes.SingleOrDefaultAsync(i => i.Id == id);
+            if (departamento == null)
+            {
+                return NotFound();
+            }
+            return View(departamento);
+        }
+
+
+        public async Task<IActionResult> Delete(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var departamento = await _context.Departamentos.SingleOrDefaultAsync(i => i.DepartamentoId == id);
+            if (departamento == null)
+            {
+                return NotFound();
+            }
+            return View(departamento);
+        }
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+
+        public async Task<ActionResult> DeleteConfirmed(long? id)
+        {
+            var departamento = await _context.Departamentos.SingleOrDefaultAsync(i => i.DepartamentoId == id);
+            _context.Departamentos.Remove(departamento);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+
+
+    }
+}
---
+++ Academico/Data/AcademicoContext.cs
@@ -0,0 +1,14 @@
+﻿using Academico.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Academico.Data
+{
+    public class AcademicoContext : DbContext
+    {
+        public AcademicoContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Instituicao> Instituicoes { get; set; }
+        public DbSet<Departamento> Departamentos { get; set; }
+    }
+}
